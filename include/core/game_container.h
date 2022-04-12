762e8bf9724d718c6585378c2b4dfaa4f02b3624
vincentdo1 <wavyfingers@gmail.com>
2022-04-12 15:30:15 -0500

    Wrote test cases. Implemented function signatures for classes

--- include/core/game_container.h
+++ include/core/game_container.h
@@ -9,20 +9,46 @@ namespace game {
 
 class GameContainer {
  public:
+    glm::vec2 box_left_dimension_;
+    glm::vec2 box_right_dimension_;
 
+    struct Obstacle {
+        public:
+            char* color_;
+            glm::vec2 position_;
+            glm::vec2 velocity_;
+            float height_;
+            float width_;
+
+            Obstacle();
+
+            Obstacle(std::string color, glm::vec2 position, glm::vec2 velocity, float height, float width);
+
+        };
     /**
-    * Displays the container walls and the current positions of the particles.
+    * Displays the container walls and the current positions of the obtsacles and the player.
     */
     void Display();
 
     /**
-     * Updates the positions and velocities of all particles (based on the rules
-     * described in the assignment documentation).
+     * Updates the positions and velocities of all obstacles and the player
      */
     void AdvanceOneFrame();
+
+    /**
+     * This function randomly creates an Obstacle
+     * @return std::vector<Obstacle>
+     */
+    std::vector<Obstacle> GenerateRandomObstacles();
+
+    GameDetails getGameDetails();
+
+    void setGameDetails(GameDetails game_details);
+
+    std::vector<Obstacle> getObstacles();
  private:
-    std::vector<std::vector<cinder::vec2>> obstacles_;
     GameDetails game_details_;
+    std::vector<Obstacle> obstacles_;
 };
 
 }  // namespace game
--- include/core/game_details.h
+++ include/core/game_details.h
@@ -2,12 +2,23 @@
 #include <string>
 #include <vector>
 #include <array>
+#include "cinder/gl/gl.h"
 
 namespace game {
 
 class GameDetails {
 public:
     float player_radius_ = 10;
+    glm::vec2 player_position_;
+    glm::vec2 player_velocity_;
+    glm::vec2 player_acceleration_;
+    bool game_over_ = false;
+
+    GameDetails();
+
+    GameDetails(float player_radius, glm::vec2 player_position, glm::vec2 player_velocity, glm::vec2 player_acceleration);
+
+
 private:
     bool invincibility_ = false;
     bool slow_time_ = false;
